3d9766446bc5b9f632d4d4add046805902faf799
Akshat <akshat.goyal1@avizva.com>
2025-09-10 04:22:45 +0530

    dfssf

--- frontend/src/pages/Tour/ToursDetails.jsx
+++ frontend/src/pages/Tour/ToursDetails.jsx
@@ -8,7 +8,7 @@ import ContactAgentModal from "../../modals/ContactAgentModal.jsx";
 import get from "lodash/get";
 import isArray from "lodash/isArray";
 import { fetchData } from "../../utils/fetchData";
-import SummaryCard from "../../components/cards/Summary/summaryCards.jsx";
+import SummaryCard from "../../components/cards/Summary/summaryCards";
 
 const TourDetails = () => {
     const { id, slug } = useParams();
